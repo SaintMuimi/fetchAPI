89511a2cb6a4b6d76785a21421f77c1dedddcb1e
SaintMuimi <148062187+SaintMuimi@users.noreply.github.com>
2024-07-06 15:52:57 +0300

    Update script.js

--- API/script.js
+++ API/script.js
@@ -7,10 +7,22 @@ fetch('https://cat-fact.herokuapp.com/facts/')
         for(let i=0; i<data.length; i++){
             const paragraph = document.createElement("p")
             paragraph.textContent = data[i].text
+            const label = document.createElement('label');
+            const  checkbox = document.createElement('input');
             root.appendChild(paragraph)
-           
+            root.appendChild(checkbox)
+
+            checkbox.type = "checkbox";
+            checkbox.name = "name";
+            checkbox.value = data[i].status.verified;
+            checkbox.id = "id";
+            checkbox.checked = data[i].status.verified;
+            
+            label.textContent = "verified" ;
+            root.appendChild(label);
+            
         }
-    })
+    } )
 
     console.log(root)
 
